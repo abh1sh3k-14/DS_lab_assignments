f01c2e36f2a28b0a6190e2858b0b9af175ef5385
abh1sh3k-14 <abhisri231106@gmail.com>
2025-08-18 16:32:26 +0530

    Update question1.cpp

--- assignment_2/question1.cpp
+++ assignment_2/question1.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 	int high,low,mid,n;
+	int index;
 	cout<<"enter the length of array you want to create: ";
 	cin>>n;
 	int arr[n];
@@ -16,6 +17,7 @@ int main(){
 	int value;
 	cout<<"enter the value you want to search in the array: ";
 	cin>>value;
+	cout<<"trying with binary search first"<<endl;
 	while(arr[mid]!=value){
 		if(arr[mid]<value){
 			low=mid;
@@ -27,4 +29,13 @@ int main(){
 		}
 	}
 	cout<<"the value was found at index "<<mid<<" of the array"<<endl;
+	cout<<"trying with linear search now"<<endl;
+	for(int i=0;i<n;i++){
+		if(arr[i]==value){
+			index=i;
+			break;
+		}
+	}
+	cout<<"the value was found at index "<<index<<" of the array"<<endl;
+	return 0;
 }
